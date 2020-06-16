6875f7d698ecdef65dc309e35d8d61ca08a7cfe6
Ajinkya Badave <badaveajinkya@gmail.com>
2020-06-16 18:39:59 +0530

    first commit

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from '../models/Todo'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  todosUrl:string =
+   'https://jsonplaceholder.typicode.com/todos?_limit=5';
+
+  constructor(private http:HttpClient) { }
+
+
+  // get todos
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.todosUrl);
+  }
+
+  // toggle completed
+  toggleCompleted(todo:Todo): Observable<any>{
+    return this.http.put(url, todo, httpOptions);
+  }
+}
